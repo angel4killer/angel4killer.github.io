7371234f0c1dfe6e1a43c3d0c5d89477dc57e049
angel4killer <79191503+angel4killer@users.noreply.github.com>
2023-10-01 00:54:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Функция для генерации ссылок
 function generateLinks(links) {
-    return links.map(link => `<a href="${link.link}" target="_blank">${link.translator}</a>`).join(', ');
+    return links.map(link => link.link).join(', ');
 }
 
 // Функция для генерации ссылки на Union
@@ -37,12 +37,12 @@ function generateUnionLink(unionLink) {
     if (unionLink.link === " — ") {
         return " — ";
     } else {
-        return `<a href="${unionLink.link}" target="_blank">${unionLink.type}</a>`;
+        return unionLink.link;
     }
 }
 
 // Функция для генерации ссылок на скачивание
 function generateDownloadLinks(links) {
-    return links.map(link => `${link.type}: <a href="${link.link}" target="_blank">Скачать</a>`).join(', ');
+    return links.map(link => `${link.type}: ${link.link}`).join(', ');
 }
 
